e3a012915715e16514b86bf0c2de759984c61eac
Alessandra Adami <adami.alessandra88@gmail.com>
2020-08-17 18:45:56 +0200

    370. Preparing Other Auth Actions

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -20,6 +20,12 @@ export interface AuthResponseData {
 
 @Injectable()
 export class AuthEffects {
+
+    @Effect()
+    authSignup = this.actions$.pipe(
+        ofType(AuthActions.SIGNUP_START)
+    );
+
     @Effect()
     authLogin = this.actions$.pipe(
         ofType(AuthActions.LOGIN_START),
@@ -36,7 +42,7 @@ export class AuthEffects {
                     map(resData => {
                         const expirationDate = new Date(
                             new Date().getTime() + +resData.expiresIn * 1000);
-                        return new AuthActions.Login({
+                        return new AuthActions.AuthenticateSuccess({
                             email: resData.email,
                             userId: resData.localId,
                             token: resData.idToken,
@@ -44,10 +50,10 @@ export class AuthEffects {
                     }),
                     catchError(errorRes => {
                         let errorMessage = 'An unknown error occurred!';
-    if (!errorRes.error || !errorRes.error.error) {
-      return of(new AuthActions.LoginFail(errorMessage));
-    }
-    switch (errorRes.error.error.message) {
+                        if (!errorRes.error || !errorRes.error.error) {
+                            return of(new AuthActions.AuthenticateFail(errorMessage));
+                        }
+                        switch (errorRes.error.error.message) {
       case 'EMAIL_EXISTS':
         errorMessage = 'This email exists already';
         break;
@@ -58,7 +64,7 @@ export class AuthEffects {
         errorMessage = 'This password is not correct.';
         break;
     }
-                        return of(new AuthActions.LoginFail(errorMessage));
+                        return of(new AuthActions.AuthenticateFail(errorMessage));
                     })
                 );
         }),
@@ -67,7 +73,7 @@ export class AuthEffects {
 
     @Effect({ dispatch: false })
     authSuccess = this.actions$.pipe(
-        ofType(AuthActions.LOGIN),
+        ofType(AuthActions.AUTHENTICATE_SUCCESS),
         tap(() => {
             this.router.navigate(['/']);
         })
